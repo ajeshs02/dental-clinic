483b0143acc27b11f6b1a001f010f5134b442a9d
Ajesh <ajeshs.dev@gmail.com>
2025-02-03 20:56:10 +0530

    sections initialised

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import { Button } from "@/components/ui/button";
+import Footer from "@/components/footer/Footer";
+import Hero from "@/components/hero/Hero";
+import Navbar from "@/components/navbar/Navbar";
+import About from "@/components/about/About";
+import Service from "@/components/services/Service";
+import Doctors from "@/components/doctors/Doctors";
+import Testimonials from "@/components/testmonials/Testimonials";
 
 export default function App() {
   return (
-    <div>
-      <Button>Hello world</Button>
+    <div className="wrapper">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Service />
+        <Doctors />
+        <Testimonials />
+      </main>
+      <Footer />
     </div>
   );
 }
