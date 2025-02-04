e61886fdd816be4b39ac4710ed5bd23927066601
adithyan7053 <adithyan7053@gmail.com>
2025-02-04 11:17:15 +0530

    added hero, about and service section

--- src/components/services/Service.tsx
+++ src/components/services/Service.tsx
@@ -1,3 +1,36 @@
-export default function Service() {
-  return <section>Service</section>;
-}
+import { services } from "@/constants/index";
+
+const Services = () => {
+  return (
+    <section className="py-12 ">
+      <div className="text-center">
+        <h5 className="text-blue-500 uppercase text-xl font-bold tracking-wider">
+          Services
+          <span className="block w-12 h-[4px] bg-gray-300 mx-auto mt-1"></span>
+        </h5>
+        <h2 className="text-6xl font-bold text-gray-900 mt-2">
+          Excellent Medical 
+        </h2>
+        <span className="text-5xl font-bold text-gray-900 pt-6">Services</span>
+      </div>
+
+      <div className="mt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+        {services.map(({ id, icon: Icon, title, description }) => (
+          <div
+            key={id}
+            className="p-8 bg-gray-100 rounded-2xl shadow-md text-center hover:shadow-lg 
+                 transition-transform duration-300 transform hover:scale-105"
+          >
+            <div className="w-20 h-20 bg-blue-500 flex items-center justify-center rounded-full mx-auto">
+              <Icon className="text-white text-4xl" />
+            </div>
+            <h3 className="mt-6 text-xl font-semibold text-gray-900">{title}</h3>
+            <p className="mt-3 text-gray-600">{description}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Services;
---
+++ src/constants/index.ts
@@ -0,0 +1,49 @@
+import { FaUserMd, FaAmbulance, FaMicroscope, FaProcedures, FaStethoscope } from "react-icons/fa";
+
+export const aboutIcons = [
+  { icon: FaUserMd, title: "Qualified", subtitle: "Doctors" },
+  { icon: FaProcedures, title: "Emergency", subtitle: "Services" },
+  { icon: FaMicroscope, title: "Accurate", subtitle: "Testing" },
+  { icon: FaAmbulance, title: "Free", subtitle: "Ambulance" },
+];
+
+
+
+export const services = [
+  {
+    id: 1,
+    icon: FaUserMd,
+    title: "Emergency Care",
+    description: "Kasd dolor no lorem nonumy sit labore tempor at justo rebun rebum stet, justo elitr dolor amet sit.",
+  },
+  {
+    id: 2,
+    icon: FaProcedures,
+    title: "Operation & Surgery",
+    description: "Kasd dolor no lorem nonumy sit labore tempor at justo rebun rebum stet, justo elitr dolor amet sit.",
+  },
+  {
+    id: 3,
+    icon: FaStethoscope,
+    title: "Outdoor Checkup",
+    description: "Kasd dolor no lorem nonumy sit labore tempor at justo rebun rebum stet, justo elitr dolor amet sit.",
+  },
+  {
+    id: 4,
+    icon: FaUserMd,
+    title: "Emergency Care",
+    description: "Kasd dolor no lorem nonumy sit labore tempor at justo rebun rebum stet, justo elitr dolor amet sit.",
+  },
+  {
+    id: 5,
+    icon: FaProcedures,
+    title: "Operation & Surgery",
+    description: "Kasd dolor no lorem nonumy sit labore tempor at justo rebun rebum stet, justo elitr dolor amet sit.",
+  },
+  {
+    id: 6,
+    icon: FaStethoscope,
+    title: "Outdoor Checkup",
+    description: "Kasd dolor no lorem nonumy sit labore tempor at justo rebun rebum stet, justo elitr dolor amet sit.",
+  },
+];
